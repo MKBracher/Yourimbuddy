a8978ae267f281d9257a13c115a79d4316b5a91d
Matthew <MKbracher02@gmail.com>
2020-08-27 22:20:14 +1000

    Added userCourse table. Still an issue with registering

--- flaskr/schema.sql
+++ flaskr/schema.sql
@@ -1,5 +1,8 @@
 DROP TABLE IF EXISTS user;
 DROP TABLE IF EXISTS question;
+DROP TABLE IF EXISTS degree;
+DROP TABLE IF EXISTS course;
+DROP TABLE IF EXISTS userCourse;
 
 CREATE TABLE user (
     id          INTEGER         PRIMARY KEY AUTOINCREMENT,
@@ -11,12 +14,12 @@ CREATE TABLE user (
     course_id   INTEGER NULL    REFERENCES course(course_id),
     phNumber    VARCHAR(20),
     email       VARCHAR(200)    UNIQUE  NOT NULL,
-    is_admin    INT             DEFAULT 0
+    is_admin    INTEGER         DEFAULT 0
 );
 
 CREATE TABLE degree (
-    degree_id   INTEGER      PRIMARY KEY AUTOINCREMENT,
-    degreeName  VARCHAR(100) UNIQUE
+    degree_id   INTEGER         PRIMARY KEY AUTOINCREMENT,
+    degreeName  VARCHAR(100)    UNIQUE
 );
 
 CREATE TABLE course (
@@ -25,6 +28,13 @@ CREATE TABLE course (
     courseName  VARCHAR(100)    UNIQUE
 );
 
+CREATE TABLE userCourse (
+    userID      INTEGER         NOT NULL,
+    courseID    INTEGER         NOT NULL,
+    PRIMARY KEY (userID, courseID)
+    FOREIGN KEY (userID)        REFERENCES user(id),
+    FOREIGN KEY (courseID)      REFERENCES course(course_id)
+);
 
 CREATE TABLE question(
     id          INTEGER         PRIMARY KEY AUTOINCREMENT,
