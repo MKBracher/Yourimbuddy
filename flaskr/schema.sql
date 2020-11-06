bc0ab2ca37aeb7b866666b5fc126cda03a5407b9
Matt W <matt.williams392@gmail.com>
2020-11-06 14:46:12 +1100

    Content in database
    Home is in the database now, might need to add more text under study essentials and FAQs subsections. Just need FAQs  page to be complete,

--- flaskr/schema.sql
+++ flaskr/schema.sql
@@ -85,7 +85,8 @@ CREATE TABLE section(
 );
 
 INSERT INTO uniPage VALUES ('1', 'Campus Services'),
-('2', 'Study Essentials');
+('2', 'Study Essentials'),
+('3', 'Home');
 
 INSERT INTO content VALUES ('1', '1', 'Your Safety On Campus:'),
 ('1', '2', 'Get in contact with us!'),
@@ -93,7 +94,10 @@ INSERT INTO content VALUES ('1', '1', 'Your Safety On Campus:'),
 ('2', '4', 'Your Degree'),
 ('2', '5', 'What a typical semester looks like'),
 ('2', '6', 'Second Hand Textbooks'),
-('2', '7', 'Exam Advice');
+('2', '7', 'Exam Advice'),
+('3', '8', 'Campus Services'),
+('3', '9', 'Study Essentials'),
+('3', '10', 'Frequently Asked Questions');
 
 INSERT INTO section VALUES ('1', '1', 'Night Security Shuttle Information', 'This service is available on all campuses and can be arranged for pickup by calling 4921 5888. It can be used to get between buildings and vehicles or to the Railway Station on request.'),
 ('2', '1', 'Safe Walk Service', 'Call 4921 5888 or email security-services@newcastle.edu.au The service provided will escort you from building to building or building to vehicle and vice versa, within the boudnaries of the campus.'),
@@ -107,4 +111,5 @@ INSERT INTO section VALUES ('1', '1', 'Night Security Shuttle Information', 'Thi
 ('10', '5', 'Your First Semester Roadmap', '1. Set yourself up for your studies with our guide for new students. Find out what you need to know, from planning your course to logging into University systems. 2. Check out our libraries. Our expert librarians can assist you with all aspects of research and learning. 3. Book a workshop. Newcastle University''s NUPrep offers FREE academic preparation and support for all University of Newcastle Students in the weeks leading up to each semester. Our courses will introduce you to learning content and completing assessment items in a similar format to your Undergraduate or Enabling Pathways program. 4. Seminars on a range of skills, including academic writing, how to research and plan an essay, and how to take better notes in class. 5. Get active with Newcastle Uni Sports & the Gym. You don’t need to splurge on a gym membership to get moving. Our gym facilities are free to use for University students or you can select to join in on our group sports!'),
 ('11', '6', '', '- Your Course Coordinators and Lecturers will supply you with a list of the textbooks, readings and other resources that are prescribed for your courses at your first lecture/class. - We strongly encourage students to wait for their first class for their lecturers to inform them of the appropriate textbooks as they may have change or may require different ones.  - From here you can choose to purchase the text online, from a book store, or from online second hand textbook sites.'),
 ('12', '7', 'Where to get program advice', 'Preparation is the key to a successful exam period, so make sure you’ve got your dates and times locked in, then plan your study based on that information. As for the best ways to study for exams, UON’s Centre for Teaching and Learning has some handy pointers: Tips for studying for exams (LINK THIS) Exam Strategies (LINK THIS) How to build a memory aid (LINK THIS)'),
-('13', '7', 'Memory Aids', 'The University of Newcastle has an open book policy for exams. Except for some exemptions, the memory aid is the minimum allowed for exams, but some courses will allow for more than the memory aid. Always check your course outlines so that you know exactly what you can bring into each exam.  Unless stated otherwise, a memory aid is 1 x A4 double-sided sheet of any type of notes, handwritten or otherwise. It can be on coloured paper and can contain diagrams.  What to bring: Most importantly, your Student ID, Driver''s License or passport to all exams, Pens or pencils, Erasers, A clear water bottle, Tissues');
+('13', '7', 'Memory Aids', 'The University of Newcastle has an open book policy for exams. Except for some exemptions, the memory aid is the minimum allowed for exams, but some courses will allow for more than the memory aid. Always check your course outlines so that you know exactly what you can bring into each exam.  Unless stated otherwise, a memory aid is 1 x A4 double-sided sheet of any type of notes, handwritten or otherwise. It can be on coloured paper and can contain diagrams.  What to bring: Most importantly, your Student ID, Driver''s License or passport to all exams, Pens or pencils, Erasers, A clear water bottle, Tissues'),
+('14', '8', '', 'Click here to go to our campus services page and find out all the amazing services that Newcastle University has to offer!');
